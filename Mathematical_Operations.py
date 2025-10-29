982e7aeb2c01f2f491055d2c598b08fa53b3f93a
SathyaPramod <sathyapramod12@gmail.com>
2025-10-29 11:19:58 +0530

    Data Types Programs

---
+++ Mathematical_Operations.py
@@ -0,0 +1,13 @@
+# print("My age: " + str(16))
+#
+# print(123 + 456)
+# print(3 - 2)
+# print(3 * 2)
+# print(5 / 2)
+# print(5 // 2)
+
+# PEMDAS - (), **, * OR /, + OR -
+print(3 * 3 + 3 / 3 - 3) # 7
+
+# Change the order and see you can get output as 3
+print(3 * (3 + 3) / 3 - 3)
