cf1c1fa67979a29ab980cf82a55867b9e58c8dc8
SathyaPramod <sathyapramod12@gmail.com>
2025-11-04 10:34:29 +0530

    Day3 and Day 4

---
+++ Day4/random_friends.py
@@ -0,0 +1,8 @@
+import random
+
+friends = ["Bharat", "Vinay", "Anand", "Rohit", "Ethan", "Siva"]
+print(random.choice(friends)) # Option 1
+
+random_index = random.randint(0, 5)
+print(friends[random_index]) # Option 2
+
